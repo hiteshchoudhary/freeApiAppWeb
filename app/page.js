2e5ab099ec3a516540d8611b3154849d4bb7835d
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2023-07-16 23:34:52 +0530

    first basic look

--- app/page.js
+++ app/page.js
@@ -1,92 +1,92 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+        <a
+            href="https://github.com/hiteshchoudhary"
+            target="_blank"
+            
+        >
+          front end | mobile dev | Backend
+        </a>
+          <code className={styles.code}></code>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://youtube.com/hiteshchoudharydotcom"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            Learn with your own APIs
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        
+        <h1 className='text-5xl text-bold'>FreeApi.app</h1> 
+        
+
       </div>
+      <a
+          href="https://youtube.com/hiteshchoudharydotcom"
+          className="bg-yellow-500 text-black font-medium p-2 px-4 rounded-lg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >A project by Hitesh Choudhary and team</a>
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="#"
+          className={styles.card}
+          
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Cooked in Docs <span>-&gt;</span>
+          </h2>
+          <p>Docs are cooked directly inside the project in easy to use swagger. With Swagger, you can read, learn and execute code within docs.</p>
+        </a>
+
+        <Link
+          href="/videos"
+          className={styles.card}
+          
+        >
+          <h2>
+            Setup videos  <span>-&gt;</span>
+          </h2>
+          <p>Setup for an open source project can be challenging. We got you covered. Visit here to access all videos that will help you to have a complete setup of the project as well as some special configuration.</p>
+        </Link>
+
+        <a
+          href="https://github.com/hiteshchoudhary"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Get project <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Project is directly available on github to access.  </p>
+          <span className='text-yellow-500 p-1 rounded'>Click here to access it.</span>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="#"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
+            Featured <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Here are some of the featured projects sent to us by users
           </p>
         </a>
       </div>
---
+++ app/videos/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Videos = () => {
+  return (
+    <div>Learn from videos</div>
+  )
+}
+
+export default Videos
